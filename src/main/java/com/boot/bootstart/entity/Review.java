24c5faebd198fd8a8e51821fbf6957e2316afa51
SergiyAgeev <sergiyageev@gmail.com>
2020-03-04 19:00:04 +0200

    created Review entity

---
+++ src/main/java/com/boot/bootstart/entity/Review.java
@@ -0,0 +1,25 @@
+package com.boot.bootstart.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productId;
+    private String userId;
+    private String profileName;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+}
